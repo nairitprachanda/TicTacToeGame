2b6472def1d24c80f41b5c2fc11348ad96ae10d7
nairitprachanda <nairitprachanda@gmail.com>
2020-10-13 10:02:32 +0530

    [nairit] refactor: creating board

---
+++ TicTacToe/TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class TicTacToeGame
+    {
+        class TicTacToe_Game
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to the TicTacToe Game!");
+                CreateBoard();
+                Console.WriteLine("Board created.");
+            }
+            static char[] CreateBoard()
+            {
+                char[] board = new char[10];
+                for (int spaceIndex = 1; spaceIndex < board.Length; spaceIndex++)
+                {
+                    board[spaceIndex] = ' ';
+                }
+                return board;
+            }
+        }
+
+    }
+}
