fbde8544e28da6cb531672a0f753c073ea227cdf
nairitprachanda <nairitprachanda@gmail.com>
2020-10-13 10:50:40 +0530

    [nairit] refactor: user chooser their letter

--- TicTacToe/TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToe/TicTacToeGame.cs
@@ -12,16 +12,29 @@ namespace TicTacToe
             {
                 Console.WriteLine("Welcome to the TicTacToe Game!");
                 CreateBoard();
-                Console.WriteLine("Board created.");
+                Console.WriteLine("Tic TaC Toe board created.");
+                ChooseUserLetter();
             }
             static char[] CreateBoard()
             {
                 char[] board = new char[10];
-                for (int spaceIndex = 1; spaceIndex < board.Length; spaceIndex++)
+                for (int i = 1; i < board.Length; i++)
                 {
-                    board[spaceIndex] = ' ';
+                    board[i] = ' ';
                 }
                 return board;
+            }
+            static char ChooseUserLetter()
+            {
+                Console.WriteLine("Please Choose your letter from X or O");
+                char UserLetter = Console.ReadLine()[0];
+                UserLetter = char.ToUpper(UserLetter);
+                while (UserLetter != 'X' && UserLetter != 'O')
+                {
+                    Console.WriteLine("Invalid move.\nChoose again from X/O: ");
+                    UserLetter = Console.ReadLine()[0];
+                }
+                return UserLetter;
             }
         }
 
