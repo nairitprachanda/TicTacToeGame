ba3673e10a11b1f841e2a90784b2a40ad63f9e53
nairitprachanda <nairitprachanda@gmail.com>
2020-10-13 12:05:16 +0530

    [nairit] refactor: user chooses first move

--- TicTacToe/TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToe/TicTacToeGame.cs
@@ -13,10 +13,11 @@ namespace TicTacToe
                 Console.WriteLine("Welcome to the TicTacToe Game!\n");
                 char[] BOARD=CreateBoard();
                 Console.WriteLine("Tic TaC Toe board created. \n");
-                ChooseUserLetter();
+                char letter=ChooseUserLetter();
+                int moveIndex = UserMove(BOARD);
                 Console.WriteLine("Showing Game Board\n");
-                ShowBoard(BOARD);
-            }
+                ShowBoard(BOARD, moveIndex, letter);
+                }
             static char[] CreateBoard()
             {
                 char[] board = new char[10];
@@ -31,21 +32,34 @@ namespace TicTacToe
                 Console.WriteLine("Please Choose your letter from X or O");
                 char UserLetter = Console.ReadLine()[0];
                 UserLetter = char.ToUpper(UserLetter);
-                while (UserLetter != 'X' && UserLetter != 'O')
+                while (UserLetter != 'X' && UserLetter != 'O' && UserLetter !='x' && UserLetter !='o')
                 {
                     Console.WriteLine("Invalid move.\nChoose again from X/O: \n");
                     UserLetter = Console.ReadLine()[0];
                 }
                 return UserLetter;
             }
-            static void ShowBoard(char [] BOARD)
+            static void ShowBoard(char [] BOARD, int moveIndex, char letter)
             {
+                BOARD[moveIndex] =letter ;
                 Console.WriteLine(BOARD[1] + "|" + BOARD[2] + "|" + BOARD[3] + "\n");
-                Console.WriteLine("________________\n");
+                Console.WriteLine("_______\n");
                 Console.WriteLine(BOARD[4] + "|" + BOARD[5] + "|" + BOARD[6] + "\n");
-                Console.WriteLine("________________\n");
+                Console.WriteLine("_______\n");
                 Console.WriteLine(BOARD[7] + "|" + BOARD[8] + "|" + BOARD[9] + "\n");
                 return;
+            }
+            static int UserMove(char[] BOARD)
+            {
+                int moveIndex;
+                Console.WriteLine("Enter index where to make move(1-9): ");
+                moveIndex = Convert.ToInt32(Console.ReadLine());
+                while (BOARD[moveIndex] != ' ')
+                {
+                    Console.WriteLine("Invalid move.\nEnter index where to make move(1-9): ");
+                    moveIndex = Convert.ToInt32(Console.ReadLine());
+                }
+                return moveIndex;
             }
         }
 
