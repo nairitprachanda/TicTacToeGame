bc3241147091df7294a0e657c6c92a01606c7215
nairitprachanda <37877904+nairitprachanda@users.noreply.github.com>
2020-10-13 10:19:09 +0530

    Update TicTacToeGame.cs

--- TicTacToe/TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToe/TicTacToeGame.cs
@@ -12,14 +12,14 @@ namespace TicTacToe
             {
                 Console.WriteLine("Welcome to the TicTacToe Game!");
                 CreateBoard();
-                Console.WriteLine("Board created.");
+                Console.WriteLine("Tic TaC Toe board created.");
             }
             static char[] CreateBoard()
             {
                 char[] board = new char[10];
-                for (int spaceIndex = 1; spaceIndex < board.Length; spaceIndex++)
+                for (int i = 1; i< board.Length; i++)
                 {
-                    board[spaceIndex] = ' ';
+                    board[i] = ' ';
                 }
                 return board;
             }
